18fc683031525f249d809ed4acbba101d9830d4a
MANDLA HEMANTH <25eg107f44@anurag.edu.in>
2025-12-26 17:04:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,27 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: true
-  },
-
-  preview: {
-    host: true,
-    port: Number(process.env.PORT) || 8080,
-    allowedHosts: [
-      ".railway.app"
-    ]
-  },
-
-  plugins: [
-    react()
-  ],
-
+export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
