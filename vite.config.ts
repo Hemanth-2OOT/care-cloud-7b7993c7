1a532cfb10529a18178b095032adcb308b73de21
MANDLA HEMANTH <25eg107f44@anurag.edu.in>
2025-12-26 16:57:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
-    host: true,
-    port: 8080,
+    host: true
   },
 
   preview: {
     host: true,
-    port: 8080,
+    port: Number(process.env.PORT) || 8080,
     allowedHosts: [
       ".railway.app"
-    ],
+    ]
   },
 
   plugins: [
-    react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
+    react()
+  ],
 
   resolve: {
     alias: {
