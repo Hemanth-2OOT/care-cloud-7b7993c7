fcc6e132388f873371740567067e8a922622c0d7
MANDLA HEMANTH <25eg107f44@anurag.edu.in>
2025-12-26 17:35:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 3000,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 3000,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
